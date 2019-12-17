d474db8973c935f3372ef04e4d8bff7880d1d81f
Klart333 <56590003+Klart333@users.noreply.github.com>
2019-12-17 14:29:00 +0100

    Coin +

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class CoinSpawner : MonoBehaviour
@@ -8,18 +9,29 @@ public class CoinSpawner : MonoBehaviour
     [SerializeField]
     GameObject coin;
 
+    int textNum = 0;
     float timer = 0;
     Rigidbody2D rb;
 
+
+    public static Text myText;
+
     void Start()
     {
-        if (gameObject.name != "Text")
+     
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        
+
+        if (gameObject.name == "Text")
         {
-            GetComponent<Rigidbody2D>().gravityScale = 0;
+            myText = GetComponent<Text>();
         }
         rb = GetComponent<Rigidbody2D>();
 
-        rb.velocity = new Vector3(-3, 0, 0);
+        if (gameObject.name != "Text")
+        {
+            rb.velocity = new Vector3(-3, 0, 0);
+        }
     }
 
     void Update()
@@ -30,5 +42,11 @@ public class CoinSpawner : MonoBehaviour
             Instantiate(coin, new Vector3(15, 0, 0), Quaternion.identity);
             timer = 0;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+
+        CoinSpawner.myText.text = textNum.ToString();
     }
 }
